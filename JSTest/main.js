fddc8550afcfc1e5f35bafaf12825a74f7ba2886
Martin Bjørnskov <mrb@martinb.eu>
2016-03-08 13:13:41 +0100

    no message

--- JSTest/main.js
+++ JSTest/main.js
@@ -2,4 +2,5 @@ var netPriceOfShoes = 500;
 var VAT = 25;
 var calculatedVAT = (1 + VAT / 100) ;
 var grossPriceOfShoes = (netPriceOfShoes * calculatedVAT);
-alert(grossPriceOfShoes);
+var amount = 5;
+alert("The price for one pair of shoes is: " + netPriceOfShoes + "\nThe price of " + amount + " pairs of shoes is: " + netPriceOfShoes * amount);
